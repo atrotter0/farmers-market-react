7ecbfe43ac3928f273395d5bed6cc150cc811cc5
Abel Trotter and Ryan Murry <ryan@>
2018-09-05 10:29:26 -0700

    Add Material icons to nav, adjust styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Toys from '@material-ui/icons/Toys';
 
 const navStyles = {
   height: '80px',
-  paddingTop: '25px',
-  paddingLeft: '20px',
+  paddingTop: '10px',
   fontSize: '2em',
   fontFamily: 'Josefin Sans',
   position: 'static'
 };
 
+const iconStyle = {
+  color: '#fff',
+  fontSize: '1.5em',
+}
+
 const Nav = () => {
   return (
     <div>
-      <AppBar style={navStyles}>Farmer's Market</AppBar>
+      <AppBar style={navStyles}>
+        <Toolbar>
+          <IconButton>
+            <Toys style={iconStyle} />
+          </IconButton>
+          Farmer's Market
+        </Toolbar>
+      </AppBar>
     </div>
   )
 }
