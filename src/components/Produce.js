35a3e46bdb3d14b027b5c83106b8e252d72eaa69
Abel Trotter and Ryan Murry <abel+ryan@>
2018-09-05 09:51:16 -0700

    add component and basic styling

---
+++ src/components/Produce.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const produceColumn = {
+  display: 'inline-block',
+  width: '80%'
+};
+
+const Produce = () => {
+  return (
+    <div style={produceColumn}>
+      Produce
+    </div>
+  );
+}
+
+export default Produce;
---
+++ src/components/Schedule.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const scheduleColumn = {
+  display: 'inline-block',
+  width: '20%'
+};
+
+const Schedule = () => {
+  return (
+    <div style={scheduleColumn}>
+      Schedule
+    </div>
+  )
+}
+
+export default Schedule;
