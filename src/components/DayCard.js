e01687f1f88b645c0e2b7fd30b453146c46b381d
Abel Trotter and Ryan Murry <abel+ryan@>
2018-09-05 11:02:45 -0700

    update card styles

--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -3,16 +3,43 @@ import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 
-
 const DayCard = (props) => {
+  const dayCardBox = {
+    width: '70%',
+    margin: '20px auto',
+    fontFamily: 'Raleway'
+  }
+
+  const dayCardHeader = {
+    margin: '10px 20px',
+    textAlign: 'center',
+    fontSize: '1.2em',
+    fontWeight: 'bold'
+  }
+
+  const dayCardContent = {
+    textAlign: 'center'
+  }
+
+  const hrStyle = {
+    width: '50%',
+    height: '2px',
+    backgroundColor: '#000',
+    border: '0px',
+    marginBottom: '20px'
+  }
   return (
     <div>
-      <Card>
+      <Card style={dayCardBox}>
         <CardContent>
-          {props.day}
-          {props.location}
-          {props.hours}
-          {props.booth}
+          <div style={dayCardHeader}>
+            {props.day}
+            <hr style={hrStyle}/>
+          </div>
+          <div style={dayCardContent}>
+            <p>{props.location} - {props.booth}</p>
+            <p>{props.hours}</p>
+          </div>
         </CardContent>
       </Card>
     </div>
