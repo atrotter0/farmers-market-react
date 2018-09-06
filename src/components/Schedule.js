0077649fdadb46c51cb9067feec31ee6fb147fff
Abel Trotter <a_trotter0@hotmail.com>
2018-09-06 12:01:23 -0700

    Extract schedule data from component

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,52 +1,14 @@
 import React from 'react';
 import DayCard from './DayCard';
 import '../styles/responsive.css';
-
-const marketSchedule = [
- {
-    day: "Sunday",
-    location: "Lents International",
-    hours: "9:00am - 2:00pm",
-    booth: "4A"
- },
- {
-    day: "Monday",
-    location: "Pioneer Courthouse Square",
-    hours: "10:00am - 2:00pm",
-    booth: "7C"
- },
- {
-    day: "Tuesday",
-    location: "Hillsboro",
-    hours: "5:00pm - 8:30pm",
-    booth: "1F"
- },
- {
-    day: "Wednesday",
-    location: "Shemanski Park",
-    hours: "10:00am - 2:00pm",
-    booth: "3E"
- },
- {
-    day: "Thursday",
-    location: "Northwest Portland",
-    hours: "2:00pm - 6:00pm",
-    booth: "6D"
- },
- {
-    day: "Saturday",
-    location: "Beaverton",
-    hours: "10:00am - 1:30pm",
-    booth: "9G"
- }
-];
+import currentSchedule from '../scheduleData';
 
 const Schedule = () => {
   const scheduleColumn = {
     maxWidth: '525px'
   };
-
   const weekDay = new Date().toLocaleString('en-us', { weekday: "long"});
+  const marketSchedule = currentSchedule();
   return (
     <div style={scheduleColumn} className='scheduleColumn'>
     {marketSchedule.map((market, index) =>
